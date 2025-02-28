74f660b5299ff8151fe8e5a2f0e75fd6beee28ba
gygab <gygab@GIGABYTE>
2025-02-28 21:11:47 +0500

    Добавьте файлы проекта.

---
+++ CardGame/Window1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CardGame
+{
+    public partial class Window1 : Window
+    {
+        public int PlayersCount { get; set; }
+
+        public Window1(int playersCount)
+        {
+            InitializeComponent();
+            PlayersCount = playersCount;
+            DataContext = this;
+        }
+    }
+}
